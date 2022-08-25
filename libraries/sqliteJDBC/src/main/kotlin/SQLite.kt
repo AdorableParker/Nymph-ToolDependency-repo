1ddbcca69b382bc3ba238fb5662ca28e7572da2a
萌萌哒帕克 <jet.piane.2010@qq.com>
2022-08-25 15:28:07 +0800

    updata

--- libraries/sqliteJDBC/src/main/kotlin/SQLite.kt
+++ libraries/sqliteJDBC/src/main/kotlin/SQLite.kt
@@ -19,29 +19,35 @@ class SQLite(private val library: Path){//} : SQLDataInterface {
      * @param sql – 要发送到数据库的 SQL 语句，通常是静态 SQL SELECT 语句
      * @return 返回执行语句后受影响的行数或是异常的原因
      */
-    inline fun <reified T> executeDQLorDCL(sql: String): SQLResult<T> {
-        return runCatching {
-            connection.createStatement().use { statement ->
-                statement.executeQuery(sql).let { resultSet ->
-                    val resultList = mutableListOf<T>()
-                    val initFunc = T::class.java.getDeclaredConstructor(*listOf(Map::class.java).toTypedArray()).apply { isAccessible = true }
-                    while (resultSet.next()) {
-                        val nameList = T::class.members.map { it.name }
-                        val row = mutableMapOf<String, Any?>()
-                        for(i in 1..resultSet.metaData.columnCount) {
-                            if (resultSet.metaData.getColumnName(i) in nameList) {
-                                row[resultSet.metaData.getColumnName(i)] = resultSet.getObject(i)
-                            }
+    inline fun <reified T> executeDQLorDCL(sql: String): SQLResult<T> = runCatching {
+        connection.createStatement().use { statement ->
+            statement.executeQuery(sql).let { resultSet ->
+                val resultList = mutableListOf<T>()
+                val initFunc = T::class.java.getDeclaredConstructor(*listOf(Map::class.java).toTypedArray()).apply { isAccessible = true }
+                while (resultSet.next()) {
+                    val nameList = T::class.members.map { it.name }
+                    val row = mutableMapOf<String, Any?>()
+                    for(i in 1..resultSet.metaData.columnCount) {
+                        if (resultSet.metaData.getColumnName(i) in nameList) {
+                            row[resultSet.metaData.getColumnName(i)] = resultSet.getObject(i)
                         }
-                        initFunc.newInstance(row).let(resultList::add)
                     }
-                    SQLResult(null, resultList)
+                    initFunc.newInstance(row).let(resultList::add)
                 }
+                SQLResult(null, resultList)
             }
-        }.onFailure {
-            return SQLResult("SQL执行异常:${it.message}\nSQL:$sql", listOf())
-        }.getOrThrow()
-    }
+        }
+    }.onFailure {
+        return SQLResult("SQL执行异常:${it.message}\nSQL:$sql", listOf())
+    }.getOrThrow()
+
+    /**
+     * 执行给定的 SQL 语句, 该语句返回单个 ResultSet 对象.
+     * @param sql – 要发送到数据库的 SQL 语句，通常是静态 SQL SELECT 语句
+     * @return 返回执行语句后受影响的行数或是异常的原因
+     */
+    private inline fun <reified T> executeDQLorDCL(sql: () -> String): SQLResult<T> = executeDQLorDCL(sql.invoke())
+
 
     /**
      * 执行给定的 SQL 语句, 它可能是 INSERT, UPDATE 或 DELETE 语句或不返回任何内容的 SQL 语句, 例如 SQL DDL 语句.
