9ba324b5728a83f4dbb218174c87e5fb8262ee58
萌萌哒帕克 <35246761+AdorableParker@users.noreply.github.com>
2023-02-14 11:07:53 +0800

    updata

--- libraries/sqliteJDBC/build.gradle.kts
+++ libraries/sqliteJDBC/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.xerial:sqlite-jdbc:3.36.0.3")
+    implementation("org.xerial:sqlite-jdbc:3.40.1.0")
     implementation(kotlin("reflect"))
 }
 
