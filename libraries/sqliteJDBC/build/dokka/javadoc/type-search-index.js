0ee2f0b947fafe4a6a813a8d1e8b0c7e19f7fb00
萌萌哒帕克 <35246761+AdorableParker@users.noreply.github.com>
2023-04-03 00:44:44 +0800

    增加安全查询

--- libraries/sqliteJDBC/build/dokka/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-var typeSearchIndex = [{"p":"[root]","l":"SQLResult","url":"[root]/SQLResult.html"}, {"p":"[root]","l":"SQLite","url":"[root]/SQLite.html"}, {"l":"All classes","url":"allclasses.html"}]
--- libraries/sqliteJDBC/src/main/kotlin/SQLiteJDBC.kt
+++ libraries/sqliteJDBC/src/main/kotlin/SQLiteJDBC.kt
@@ -17,6 +17,46 @@ class SQLiteJDBC(private val library: Path) {
 
     /**
      * 执行给定的 SQL 语句, 该语句返回单个 ResultSet 对象.
+     * @param safeSQL – 要发送到数据库的 SQL 语句，通常是静态 SQL SELECT 语句
+     * @param values – 要传入 SQL 语句的参数
+     * @return 返回执行语句后受影响的行数或是异常的原因
+     */
+    inline fun <reified T> safeExecuteDQLorDCL(safeSQL: String, vararg values: Any): SQLResult<T> = runCatching {
+        connection.prepareStatement(safeSQL).use { stmt ->
+            // 设置参数
+            values.forEachIndexed { index, value ->
+                when (value) {
+                    is String -> stmt.setString(index + 1, value)
+                    is Long -> stmt.setLong(index + 1, value)
+                    is Double -> stmt.setDouble(index + 1, value)
+                    is Int -> stmt.setInt(index + 1, value)
+                    is Boolean -> stmt.setBoolean(index + 1, value)
+                }
+            }
+            stmt.executeQuery().let { resultSet ->
+                val resultList = mutableListOf<T>()
+                val initFunc = T::class.java.getDeclaredConstructor(*listOf(Map::class.java).toTypedArray())
+                    .apply { isAccessible = true }
+                while (resultSet.next()) {
+                    val nameList = T::class.members.map { it.name }
+                    val row = mutableMapOf<String, Any?>()
+                    for (i in 1..resultSet.metaData.columnCount) {
+                        if (resultSet.metaData.getColumnName(i) in nameList) {
+                            row[resultSet.metaData.getColumnName(i)] = resultSet.getObject(i)
+                        }
+                    }
+                    initFunc.newInstance(row).let(resultList::add)
+                }
+                SQLResult(null, resultList)
+            }
+        }
+    }.onFailure {
+        return SQLResult("SQL执行异常:${it.message}\nSQL:$safeSQL", listOf())
+    }.getOrThrow()
+
+
+    /**
+     * 执行给定的 SQL 语句, 该语句返回单个 ResultSet 对象.
      * @param sql – 要发送到数据库的 SQL 语句，通常是静态 SQL SELECT 语句
      * @return 返回执行语句后受影响的行数或是异常的原因
      */
